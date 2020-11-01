5ed96f66f9ffc57f5006e02846efe12bc4132c7d
Roman Zhyhulskyi <roman.zhyhulskyi@valtech.com>
2020-11-01 14:34:40 +0200

    added light endpoint

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
 
+app.get('/light', (req, res) => {
+	res.send('Light');
+});
+
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`);
 });
